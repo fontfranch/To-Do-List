77dc74372fb3bb9e34f8ca62c844b480bf7986a3
Avelino Font <fontfranch@gmail.com>
2015-01-26 13:18:31 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,11 +11,40 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        #Acceder a la
+        #webpage
         self.browser.get('http://localhost:8000')
 
+        #Ver como se llama el sitio web
         self.assertIn('To-Do',self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        #Descubrir la posibilidad de escribir un elemento
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+            )
+
+        #Escribir "Buy peacock feathers"
+        #dentro de la caja de texto
+        inputbox.send_keys('Buy peacock feathers')
+
+        #Darle al 'intro' y enviar
+        #el texto
+        inpubox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: Buy peacock feathers' for row in rows)
+            )
+
+        
         self.fail('Finish the test')
 
+        
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
