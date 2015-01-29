a5fa63d0c97e0166a9518243dabd54bfac51f8cc
Avelino Font Franch <fontfranch@gmail.com>
2015-01-29 21:08:38 +0100

    Giving the app a REST style

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -41,6 +41,8 @@ class NewVisitorTest(LiveServerTestCase):
         #Darle al 'intro' y enviar
         #el texto
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/+')
 
         #Introducir un nuevo elemento en la lista
         #'Use peacock feathers to make a fly'
@@ -54,9 +56,45 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        
+
+        #nuevo usuario, Francias el del Actual
+
+        #Vamos cerrarlo todo para que no haya problemas
+        #entre un usuario y otro
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        #Francis entra a la principal, no debe haber restos de la
+        #anterior usuaria, Edith.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        #Francis comienza una nueva lista
+        #Es menos interesante que Edith
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        #Ahora Francis tiene una URL exclusiva
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+
+        #Volvemos a comprobar que no hay rastro de Edith
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertnotIn('Buy peacock feathers',page_text)
+        self.assertIn('Buy milk', page_text)
+
+        #Satisfecho, ambos se acuestan
         
         self.fail('Finish the test')
+
+        #visita a la web, la lista sigue en su sitio
+
+        #fin de la visita.
 
 
 
