40c0514ca70ca5ef50cc861111b7c24a65f2736f
fontfranch <fontfranch@gmail.com>
2015-02-19 13:46:04 +0100

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,8 +3,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+    
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -20,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Acceder a la
         #webpage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         #Ver como se llama el sitio web
         self.assertIn('To-Do',self.browser.title)
@@ -67,7 +82,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         #Francis entra a la principal, no debe haber restos de la
         #anterior usuaria, Edith.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -100,7 +115,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
 
         #Edith va la pagina principal
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         #Se da cuenta de que la entrada de datos esta entrada
